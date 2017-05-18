d9fdba91f4c3375af2cd7e99fbb850fd010569f7
Alexander Chan <alexander-chan@users.noreply.github.com>
2017-05-18 16:18:55 -0700

    Update - Code works but looks ugly

--- CleanStat/Q2Q3_Sankey_Diagram.R
+++ CleanStat/Q2Q3_Sankey_Diagram.R
@@ -13,25 +13,31 @@ q2q3 <- merge(q2, q3, by = "NameInt")
 q2q3 <- q2q3[,c("NameInt","Q2_CS_Score_Round", "GridScrRounded")]
 q2q3 %>% head
 
-q2q3$q2q3_change <- paste(q2q3$Q2_CS_Score_Round %>% as.character(), q2q3$GridScrRounded %>% as.character(), sep ="")
+q2q3$q2q3_change <- paste(q2q3$Q2_CS_Score_Round %>% as.character(), q2q3$GridScrRounded %>% as.character(), sep = "")
 q2q3 %>% head
 transition <- table(q2q3$q2q3_change)
 transition
 
 install.packages("networkD3")
 library(networkD3)
-?sankeyNetwork
 nodes <- data.frame(c("Q2 #1's", "Q2 #2's", "Q2 #3's", "Q3 #1's", "Q3 #2's", "Q3 #3's"))
-names(nodes) <- "nodes"
+#nodes <- data.frame(c('Q2_1','Q2_2','Q2_3','Q3_1','Q3_2','Q3_3'))
+#names(nodes) <- "nodes"
+names(nodes) <- "name"
+nodes$name <- as.character(nodes$name)
 nodes
 
-links <- data.frame(source = c(rep("1", 3), rep("2", 3), rep("3", 3)),target = rep(1:3, 3) )
-links$value <- transition
-links
-nodes
 
-cleanstat <- list(nodes, links)
-str(cleanstat)
+links <- data.frame(source = c(rep("1", 3), rep("2", 3), rep("3", 3)), target = rep(1:3, 3) )
+links$value <- as.integer(transition)
+links$source <- as.integer(paste(links$source))
+
+
+cleanstat <- list(nodes = nodes, links = links)
 
 sankeyNetwork(Links = cleanstat$links, Nodes = cleanstat$nodes, Source = 'source',
-              Target = 'target', Value = 'value', NodeID = 'nodes')
+              Target = 'target', Value = 'value', NodeID = 'name')
+
+
+
+
