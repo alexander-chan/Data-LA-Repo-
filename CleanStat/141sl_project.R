12dbb334429b0c842a8b2107c6484451efa0f80d
jon-a-a <jonaa@ucla.edu>
2017-05-18 14:36:31 -0700

    not scraping all datasets for some reason so it breaks some things, if you have the q1-q3 grid datasets (1100+ rows in each) you can still use the code to merge

--- CleanStat/141sl_project.R
+++ CleanStat/141sl_project.R
@@ -1,8 +1,9 @@
 library(RSocrata)
 library(readr)
+library(dplyr)
 
 #scrape website
-clean <- ls.socrata("http://geohub.lacity.org/datasets?q=cleanstat")
+clean <- ls.socrata("http://geohub.lacity.org/datasets")
 
 #no longer scraping the clean datasets (?), scrapes all datasets from lacity.org/datasets
 
@@ -32,7 +33,29 @@ quarter_function <- function(x) {
 q1 <- quarter_function("16 Q1")
 q2 <- quarter_function("16 Q2")
 q3 <- quarter_function("16 Q3")
-q4 <- quarter_function("16 Q4") #q4 will fail since we don't have access to grid data, only web map if you look at website
+#q4 <- quarter_function("16 Q4") #q4 will fail since we don't have access to grid data, only web map if you look at website
+
+names(q2)[35] <- "score_round_q2" #Q2_CS_Score_Round
+names(q3)[14] <- "score_round_q3" #score_round_q3
+
+q1$CSScore_oringal <- q1$CSScore #save original scores
+index <- which(q1$CSScore < 1.5) #manually round
+q1$CSScore[index] <- 1
+index <- which(q1$CSScore < 2.5 & q1$CSScore >1)
+q1$CSScore[index] <- 2
+index <- which(q1$CSScore > 2.5)
+q1$CSScore[index] <- 3
+
+
+#all NameInt
+names(q2)[9]
+names(q3)[2]
+names(q1)[10]
+
+merge <- inner_join(q2, q3, by = "NameInt")
+
+merge <- inner_join(q1, merge, by = "NameInt")
+
 
 
 
