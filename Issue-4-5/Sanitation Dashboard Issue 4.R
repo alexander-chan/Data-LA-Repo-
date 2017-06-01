21b258095cfca143163af16677a101f03980db2d
Alexander Chan <alexander-chan@users.noreply.github.com>
2017-05-31 18:22:55 -0700

    Modified month colors

--- Issue-4-5/Sanitation Dashboard Issue 4.R
+++ Issue-4-5/Sanitation Dashboard Issue 4.R
@@ -1,14 +1,20 @@
 library(readr)
-miles_sewage_cleaned <- read_csv('C://Users//Conor//Downloads//LASAN__Miles_of_Sewer_Cleaned.csv')
+miles_sewage_cleaned <- read_csv('LASAN__Miles_of_Sewer_Cleaned.csv')
 miles_sewage_cleaned$`Calendar Year` <- ifelse(miles_sewage_cleaned$`Month Number` %in% c(1:6), 
                                                miles_sewage_cleaned$`Fiscal Year` - 1,
                                                miles_sewage_cleaned$`Fiscal Year`)
 month_names <- c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
 miles_sewage_cleaned$`Month Factor` <- factor(miles_sewage_cleaned$Month, levels = rev(month_names))
 
+
+colors <- c("#1a1334",  "#26294a" , "#01545a",    
+            "#017351",    "#03c383",    "#aad962",
+            "#fbbf45",    "#ef6a32",    "#ed0345",   
+            "#a12a5e",    "#710162",    "#110141")   
+
 library(ggplot2)
 ggplot(data = miles_sewage_cleaned, aes(`Fiscal Year`)) + 
-  geom_bar(aes(weight = `Miles of Sewer Cleaned`, fill = `Month Factor`)) +
+  geom_bar(aes(weight = `Miles of Sewer Cleaned`), fill = "#7bccc4") +
   coord_flip() + 
   theme_bw() +
   ggtitle('Bar Chart of Miles Cleaned by Fiscal Year and Month')
@@ -17,10 +23,12 @@ ggplot(data = miles_sewage_cleaned, aes(`Calendar Year`)) +
   geom_bar(aes(weight = `Miles of Sewer Cleaned`, fill = `Month Factor`)) +
   coord_flip()+
   theme_bw() +
-  ggtitle('Bar Chart of Miles Cleaned by Calendar Year and Month')
+  ggtitle('Bar Chart of Miles Cleaned by Calendar Year and Month') +
+  scale_fill_manual(values=colors)
 
 ggplot(data = miles_sewage_cleaned[miles_sewage_cleaned$`Calendar Year` %in% c(2014:2016),], aes(Month)) +
   geom_bar(aes(weight = `Miles of Sewer Cleaned`, fill = Month)) +
   theme_bw() +
   ggtitle('Bar Chart of Miles Cleaned by Month for Calendar Years between 2014-2016') +
-  scale_x_discrete(limits = month_names)
+  scale_x_discrete(limits = month_names)+
+  scale_fill_manual(values=colors)
