e95df9dced1f8ecfd95bc923f2a112e5f18788b7
jon-a-a <jonaa@ucla.edu>
2017-05-31 18:56:14 -0700

    fixed level errors

--- Issue-4-5/Sanitation Dashboard Issue 5.R
+++ Issue-4-5/Sanitation Dashboard Issue 5.R
@@ -5,9 +5,8 @@ sewer_overflow$`Calendar Year` <- ifelse(sewer_overflow$`Month Number` %in% c(1:
                                          sewer_overflow$`Fiscal Year`)
 month_names <- c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
 sewer_overflow$`Month Factor` <- factor(sewer_overflow$Month, levels = rev(month_names))
+sewer_overflow$`Month Number Factor` <- factor(sewer_overflow$`Month Number`, levels = 12:1)
 
-
-library(RColorBrewer)
 colors <- c("#1a1334",  "#26294a" , "#01545a",    
   "#017351",    "#03c383",    "#aad962",
   "#fbbf45",    "#ef6a32",    "#ed0345",   
@@ -15,21 +14,22 @@ colors <- c("#1a1334",  "#26294a" , "#01545a",
 
 library(ggplot2)
 ggplot(data = sewer_overflow, aes(`Fiscal Year`)) +
-  geom_bar(aes(weight = `Sanitary Sewer Overflows`), fill = "#7bccc4") +
+  geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Number Factor`)) +
   coord_flip() +
   theme_bw() +
-  ggtitle('Bar Chart of Overflow by Fiscal Year and Month')
+  ggtitle('Overflow Bar Chart by Fiscal Year and Month') +
+  scale_fill_manual(values=colors)
 
 ggplot(data = sewer_overflow, aes(`Calendar Year`)) +
   geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Factor`)) +
   coord_flip() +
   theme_bw() +
-  ggtitle('Bar Chart of Overflow by Calendar Year and Month')+
+  ggtitle('Overflow Bar Chat by Calendar Year and Month')+
   scale_fill_manual(values=colors)
   
-ggplot(data = sewer_overflow[sewer_overflow$`Calendar Year` %in% c(2015:2016),], aes(`Month Number`)) +
-  geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = Month)) +
+ggplot(data = sewer_overflow[sewer_overflow$`Calendar Year` %in% c(2015:2016),], aes(`Month Factor`)) +
+  geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Factor`)) +
   theme_bw() +
-  ggtitle('Bar Chart of Overflow by Month for Calendar Years 2015-2016') +
+  ggtitle('Overflow Bar Chart by Month for Calendar Years 2015-2016') +
   scale_x_discrete(limits = month_names) +
   scale_fill_manual(values=colors)
