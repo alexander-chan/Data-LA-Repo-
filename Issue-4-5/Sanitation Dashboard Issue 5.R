458b585daeb85883c552efde3134578c66a13a96
jon-a-a <jonaa@ucla.edu>
2017-05-31 18:06:53 -0700

    changed colors for bar charts

--- Issue-4-5/Sanitation Dashboard Issue 5.R
+++ Issue-4-5/Sanitation Dashboard Issue 5.R
@@ -1,26 +1,40 @@
 library(readr)
-sewer_overflow <- read_csv('C://Users//Conor//Downloads//LASAN___Sanitary_Sewer_Overflows.csv')
+sewer_overflow <- read_csv('C://Users//jon//Downloads//LASAN___Sanitary_Sewer_Overflows.csv')
 sewer_overflow$`Calendar Year` <- ifelse(sewer_overflow$`Month Number` %in% c(1:6), 
                                          sewer_overflow$`Fiscal Year` - 1,
                                          sewer_overflow$`Fiscal Year`)
 month_names <- c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
 sewer_overflow$`Month Factor` <- factor(sewer_overflow$Month, levels = rev(month_names))
 
+
+library(RColorBrewer)
+colors <- c("#1a1334",  "#26294a" , "#01545a",    
+  "#017351",    "#03c383",    "#aad962",
+  "#fbbf45",    "#ef6a32",    "#ed0345",   
+  "#a12a5e",    "#710162",    "#110141")   
+  
+names(colors) <- levels(sewer_overflow$`Month Factor`)
+colScale <- scale_colour_manual(name = "`Month Factor`",values = colors)
+
 library(ggplot2)
 ggplot(data = sewer_overflow, aes(`Fiscal Year`)) +
-  geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Factor`)) +
+  geom_bar(aes(weight = `Sanitary Sewer Overflows`), fill = "#7bccc4") +
   coord_flip() +
   theme_bw() +
-  ggtitle('Bar Chart of Overflow by Fiscal Year and Month')
+  ggtitle('Bar Chart of Overflow by Fiscal Year and Month') +
+  scale_fill_brewer(type = "qual", direction = -1, palette = "Paired")
+
 
 ggplot(data = sewer_overflow, aes(`Calendar Year`)) +
   geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Factor`)) +
   coord_flip() +
   theme_bw() +
-  ggtitle('Bar Chart of Overflow by Calendar Year and Month')
+  ggtitle('Bar Chart of Overflow by Calendar Year and Month')+
+  scale_fill_brewer(type = "qual", direction = -1, palette = "Paired")
 
 ggplot(data = sewer_overflow[sewer_overflow$`Calendar Year` %in% c(2015:2016),], aes(`Month Number`)) +
   geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = Month)) +
   theme_bw() +
   ggtitle('Bar Chart of Overflow by Month for Calendar Years 2015-2016') +
-  scale_x_discrete(limits = month_names)
+  scale_x_discrete(limits = month_names) +
+  scale_fill_brewer(type = "qual", direction = -1, palette = "Paired")
