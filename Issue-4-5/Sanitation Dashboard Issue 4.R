a8c9ab2b4c2c0f351f9423b29daa1664d213899e
jon-a-a <jonaa@ucla.edu>
2017-06-03 15:09:10 -0700

    added comments and ggplotly

--- Issue-4-5/Sanitation Dashboard Issue 4.R
+++ Issue-4-5/Sanitation Dashboard Issue 4.R
@@ -1,40 +1,58 @@
 library(readr)
+#read in data
 miles_sewage_cleaned <- read_csv('LASAN__Miles_of_Sewer_Cleaned.csv')
+
+#creating calendar year variable
 miles_sewage_cleaned$`Calendar Year` <- ifelse(miles_sewage_cleaned$`Month Number` %in% c(1:6), 
                                                miles_sewage_cleaned$`Fiscal Year` - 1,
                                                miles_sewage_cleaned$`Fiscal Year`)
+#creating month names
 month_names <- c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
+
+#creating month factor (calendar months)
 miles_sewage_cleaned$`Month Factor` <- factor(miles_sewage_cleaned$Month, levels = rev(month_names))
+
+#creating month number factor (fiscal months)
 miles_sewage_cleaned$`Month Number Factor` <- factor(miles_sewage_cleaned$`Month Number`, levels = 12:1)
 
+#blue gradient
+#colors <- c("#2C00E5","#2811DE","#2422D8","#2034D1","#1C45CB","#1856C4","#1468BE","#1079B7","#0C8AB1","#089CAA","#04ADA4","#00BF9E")
 
 
+#rainbow colors
 colors <- c("#1a1334",  "#26294a" , "#01545a",    
             "#017351",    "#03c383",    "#aad962",
             "#fbbf45",    "#ef6a32",    "#ed0345",   
             "#a12a5e",    "#710162",    "#110141")   
 
+#fiscal year
 library(ggplot2)
-ggplot(data = miles_sewage_cleaned, aes(`Fiscal Year`)) + 
+fiscal <- ggplot(data = miles_sewage_cleaned, aes(`Fiscal Year`)) + 
   geom_bar(aes(weight = `Miles of Sewer Cleaned`, fill = `Month Number Factor`)) +
-  coord_flip() + 
   theme_bw() +
   ggtitle('Bar Chart of Miles Cleaned by Fiscal Year and Month')+
   scale_fill_manual(values=colors)+
-  guides(fill = guide_legend(reverse=TRUE))
+  guides(fill = guide_legend(reverse = TRUE))
 
-ggplot(data = miles_sewage_cleaned, aes(`Calendar Year`)) +
+#calendar year
+calendar <- ggplot(data = miles_sewage_cleaned, aes(`Calendar Year`)) +
   geom_bar(aes(weight = `Miles of Sewer Cleaned`, fill = `Month Factor`)) +
-  coord_flip()+
   theme_bw() +
   ggtitle('Bar Chart of Miles Cleaned by Calendar Year and Month') +
-  scale_fill_manual(values=colors) +
-  guides(fill = guide_legend(reverse=TRUE))
+  scale_fill_manual(values=colors)+
+  guides(fill = guide_legend(reverse = TRUE))
 
-ggplot(data = miles_sewage_cleaned[miles_sewage_cleaned$`Calendar Year` %in% c(2014:2016),], aes(`Month Factor`)) +
+#monthly
+monthly <- ggplot(data = miles_sewage_cleaned[miles_sewage_cleaned$`Calendar Year` %in% c(2014:2016),], aes(`Month Factor`)) +
   geom_bar(aes(weight = `Miles of Sewer Cleaned`, fill = `Month Factor`)) +
   theme_bw() +
   ggtitle('Bar Chart of Miles Cleaned by Month for Calendar Years between 2014-2016') +
   scale_x_discrete(limits = month_names)+
   scale_fill_manual(values=colors)+
-  guides(fill = guide_legend(reverse=TRUE))
+  guides(fill = guide_legend(reverse = TRUE))
+
+#convert to ggploty
+library(plotly)
+plotly::ggplotly(fiscal)
+plotly::ggplotly(calendar)
+plotly::ggplotly(monthly)
--- Issue-4-5/Sanitation Dashboard Issue 5.R
+++ Issue-4-5/Sanitation Dashboard Issue 5.R
@@ -1,38 +1,54 @@
 library(readr)
+#read in data
+sewer_overflow <- read_csv('C://Users//jon//Downloads//LASAN___Sanitary_Sewer_Overflows.csv')
+
+#creating calendar year variable
 sewer_overflow <- read_csv('LASAN___Sanitary_Sewer_Overflows.csv')
+
 sewer_overflow$`Calendar Year` <- ifelse(sewer_overflow$`Month Number` %in% c(1:6), 
                                          sewer_overflow$`Fiscal Year` - 1,
                                          sewer_overflow$`Fiscal Year`)
+#creating month names
 month_names <- c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
+
+#creating month factor (calendar months)
 sewer_overflow$`Month Factor` <- factor(sewer_overflow$Month, levels = rev(month_names))
+
+#creating month number factor (fiscal months)
 sewer_overflow$`Month Number Factor` <- factor(sewer_overflow$`Month Number`, levels = 12:1)
 
+
+#rainbow colors
 colors <- c("#1a1334",  "#26294a" , "#01545a",    
-  "#017351",    "#03c383",    "#aad962",
-  "#fbbf45",    "#ef6a32",    "#ed0345",   
-  "#a12a5e",    "#710162",    "#110141")   
+            "#017351",    "#03c383",    "#aad962",
+            "#fbbf45",    "#ef6a32",    "#ed0345",   
+            "#a12a5e",    "#710162",    "#110141")   
 
 library(ggplot2)
-ggplot(data = sewer_overflow, aes(`Fiscal Year`)) +
+fiscal <- ggplot(data = sewer_overflow, aes(`Fiscal Year`)) +
   geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Number Factor`)) +
-  coord_flip() +
   theme_bw() +
   ggtitle('Overflow Bar Chart by Fiscal Year and Month') +
   scale_fill_manual(values=colors)+
   guides(fill = guide_legend(reverse=TRUE))
 
-ggplot(data = sewer_overflow, aes(`Calendar Year`)) +
+calendar <- ggplot(data = sewer_overflow, aes(`Calendar Year`)) +
   geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Factor`)) +
-  coord_flip() +
   theme_bw() +
   ggtitle('Overflow Bar Chat by Calendar Year and Month')+
   scale_fill_manual(values=colors)+
   guides(fill = guide_legend(reverse=TRUE))
-  
-ggplot(data = sewer_overflow[sewer_overflow$`Calendar Year` %in% c(2015:2016),], aes(`Month Factor`)) +
+
+monthly <- ggplot(data = sewer_overflow[sewer_overflow$`Calendar Year` %in% c(2015:2016),], aes(`Month Factor`)) +
   geom_bar(aes(weight = `Sanitary Sewer Overflows`, fill = `Month Factor`)) +
   theme_bw() +
   ggtitle('Overflow Bar Chart by Month for Calendar Years 2015-2016') +
   scale_x_discrete(limits = month_names) +
   scale_fill_manual(values=colors)+
-  guides(fill = guide_legend(reverse=TRUE))
+  guides(fill = guide_legend(reverse = TRUE))
+
+#convert to ggplotly
+library(plotly)
+ggplotly(fiscal)
+ggplotly(calendar)
+ggplotly(monthly)
