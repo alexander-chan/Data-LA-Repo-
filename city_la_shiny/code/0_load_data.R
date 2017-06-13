c0d34e1c0aa9eda4a78959945912602ddaf2da20
Conor McNamara <conor.s.mcnamara@gmail.com>
2017-06-12 23:43:44 -0700

    Create 0_load_data.R

--- city_la_shiny/code/0_load_data.R
+++ city_la_shiny/code/0_load_data.R
@@ -5,6 +5,8 @@ q3 <- read.csv("./city_LA_shiny/data/LACityCleanStat_Q32016_Grids.csv")
 
 # issue 4 data
 miles_sewage_cleaned <- read_csv('./city_LA_shiny/data/LASAN__Miles_of_Sewer_Cleaned.csv')
+#miles_sewage_cleaned <- read.socrata("https://data.lacity.org/A-Livable-and-Sustainable-City/LASAN-Miles-of-Sewer-Cleaned/iyyp-p2fx")
 
 # issue 5 data
 sewer_overflow <- read_csv('./city_LA_shiny/data/LASAN___Sanitary_Sewer_Overflows.csv')
+#sewer_overflow <- read.socrata("https://data.lacity.org/A-Livable-and-Sustainable-City/LASAN-Sanitary-Sewer-Overflows/cynx-j8d5")
