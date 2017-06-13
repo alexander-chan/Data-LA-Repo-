e52afa11dc0845b854c766d0e9038450c2ae60fb
Conor McNamara <conor.s.mcnamara@gmail.com>
2017-06-13 01:10:25 -0700

    Update 0_packages.R

--- city_la_shiny/code/0_packages.R
+++ city_la_shiny/code/0_packages.R
@@ -5,7 +5,7 @@
 
 pkgs <- c("shiny", "shinydashboard", "tidyverse", "stringr", 
           "lubridate", "plotly", "leaflet", "googleVis", "zoo",
-          "dplyr", "ggmap", "readr","RSocrata","gsheet", "reshape2",
+          "dplyr", "readr","RSocrata","gsheet", "reshape2",
          "devtools")
 sapply(pkgs, require, character.only = TRUE)
 devtools::install_github('bhaskarvk/leaflet.extras')
