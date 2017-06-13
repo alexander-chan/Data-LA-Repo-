894320dfeaba4e29cd6e83f31fc2635d46893201
Conor McNamara <conor.s.mcnamara@gmail.com>
2017-06-13 00:29:02 -0700

    Create 0_packages.R

--- city_la_shiny/code/0_packages.R
+++ city_la_shiny/code/0_packages.R
@@ -1,4 +1,8 @@
 # load pacakges
+
+#Note, leaflet.extras is not supported on CRAN, need to use installr
+
+devtools::install_github('bhaskarvk/leaflet.extras')
 pkgs <- c("shiny", "shinydashboard", "tidyverse", "stringr", 
           "lubridate", "plotly", "leaflet", "googleVis", "zoo",
           "dplyr", "ggmap", "readr","RSocrata","gsheet", "reshape2",
