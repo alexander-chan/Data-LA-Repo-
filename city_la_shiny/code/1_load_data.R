c2a1affc543fe4df4d69617e27cf955f26eb96b2
Min Woo Sun <minwoosun55@gmail.com>
2017-06-13 07:48:43 -0700

    load data q4

--- city_la_shiny/code/1_load_data.R
+++ city_la_shiny/code/1_load_data.R
@@ -2,6 +2,7 @@
 # sankey diagram data
 q2 <- read.csv("./city_LA_shiny/data/Clean_Streets_Index_Grids_2016_Q2.csv")
 q3 <- read.csv("./city_LA_shiny/data/LACityCleanStat_Q32016_Grids.csv")
+q4 <- read.csv("./city_LA_shiny/data/q4.csv")
 
 # issue 4 data
 miles_sewage_cleaned <- read_csv('./city_LA_shiny/data/LASAN__Miles_of_Sewer_Cleaned.csv')
