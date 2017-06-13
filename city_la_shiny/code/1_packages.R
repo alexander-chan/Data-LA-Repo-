abc445d6f767c07fc60d86a048f476c412920a20
Conor McNamara <conor.s.mcnamara@gmail.com>
2017-06-12 19:50:23 -0700

    Create 1_packages.R

--- city_la_shiny/code/1_packages.R
+++ city_la_shiny/code/1_packages.R
@@ -1,7 +1,8 @@
 # load pacakges
 pkgs <- c("shiny", "shinydashboard", "tidyverse", "stringr", 
           "lubridate", "plotly", "leaflet", "googleVis", "zoo",
-          "dplyr", "ggmap", "readr","RSocrata","gsheet", "reshape2")
+          "dplyr", "ggmap", "readr","RSocrata","gsheet", "reshape2",
+         "leaflet.extras")
 sapply(pkgs, require, character.only = TRUE)
 
 #library(shinyjs)
